a1eda481d30e576b131685d3e2bfd5b6b5435d9b
Stephen Binns <stephen.binns@gmail.com>
2015-02-03 18:57:12 +0000

    Fixed bouncer

--- lib/walker.rb
+++ lib/walker.rb
@@ -1,3 +1,14 @@
+class Enemy < GameObject
+  def check_collides_with_block
+    tiles = self.game_state.tiles.tiles_around_object(self)
+    self.each_collision(tiles) do | me, tile |
+      if tile.instance_of? Block
+        self.y = tile.bb.top-1
+      end
+    end
+  end
+end
+
 class Walker < Enemy
   trait :bounding_box, :debug => true
   traits :collision_detection, :velocity, :timer
@@ -49,24 +60,15 @@ class Walker < Enemy
   end
 end
 
-class Enemy < GameObject
-  def check_collides_with_block
-    tile = self.game_state.tiles.tile_at_object(self)
-    if tile.instance_of? Block
-      self.y = tile.bb.top-1
-    end
-  end
-end
-
 class Bouncer < Enemy
   trait :bounding_box, :debug => true
   traits :collision_detection, :velocity, :timer
 
   def setup
     @animations = Chingu::Animation.new(:file => "enemies_16x16.png")
-    @animations.frame_names = { :still => 4..4, :bouncer => 5..7 }
+    @animations.frame_names = { :bouncer => 4..7 }
+    @animation = @animations[:bouncer]
     
-    @animation = @animations[:still]
     self.zorder = 300
     self.max_velocity = 10
     self.acceleration_y = 0.5 # gravity!
@@ -74,24 +76,16 @@ class Bouncer < Enemy
 
     update
     cache_bounding_box
+
+    every(500) {
+      self.velocity_y = -7
+    }
   end
 
   def update
     @image = @animation.next
     return unless self.game_state.viewport.inside? self
-    check_collides_with_block
 
-    every(500) {
-      @animation = @animations[:bouncer]
-      tile = self.game_state.tiles.tile_at_object(self, :below)
-      if tile != @last_tile
-        puts tile
-      end
-      @last_tile = tile
-      if tile.instance_of? Block
-        self.velocity_y = -1
-        puts 'boing'
-      end
-    }
+    check_collides_with_block
   end
 end
